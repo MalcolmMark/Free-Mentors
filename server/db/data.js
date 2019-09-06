3455eb5559b8f8b60b6f1e143f128c852839a5be
MalcolmMark <45765235+MalcolmMark@users.noreply.github.com>
2019-09-06 05:52:20 +0300

    ft(UI): get all mentors (#17)
    - add get all metor routes

--- server/db/data.js
+++ server/db/data.js
@@ -1,4 +1,6 @@
 import { User } from "../models/user.model";
+import { Session } from "../models/session.model";
+import { Review } from "../models/review.model";
 /* User Data */
 
 export let users = [
@@ -6,6 +8,8 @@ export let users = [
     new User(2, 'moise@gmail.com', 'Koko', 'Ange', '$2a$10$Skf6psO9yOsoG/FMZ2gAzeoFkAyRTM/G3Y8pMnQ9.NHx21YeIttxO', 'Miduha', 'I am ...', 'Developer', 'Swift', 'mentee'),
     new User(3, 'ezra@gmail.com', 'Koko', 'Ange', '$2a$10$Skf6psO9yOsoG/FMZ2gAzeoFkAyRTM/G3Y8pMnQ9.NHx21YeIttxO', 'Miduha', 'I am ...', 'Developer', 'Swift', 'mentee'),
     new User(4, '@gmail.com', 'Koko', 'Ange', '$2a$10$Skf6psO9yOsoG/FMZ2gAzeoFkAyRTM/G3Y8pMnQ9.NHx21YeIttxO', 'Miduha', 'I am ...', 'Developer', 'Swift', 'mentee'),
+    new User(5, '@gmail.com', 'Koko', 'Ange', '$2a$10$Skf6psO9yOsoG/FMZ2gAzeoFkAyRTM/G3Y8pMnQ9.NHx21YeIttxO', 'Miduha', 'I am ...', 'Developer', 'Swift', 'mentor'),
+    new User(6, '@gmail.com', 'Koko', 'Ange', '$2a$10$Skf6psO9yOsoG/FMZ2gAzeoFkAyRTM/G3Y8pMnQ9.NHx21YeIttxO', 'Miduha', 'I am ...', 'Developer', 'Swift', 'mentor'),
 ];
 
 /* eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2Vrb0BnbWFpbC5jb20iLCJpYXQiOjE1NjczNjczMjd9.hH3utDCmPjVZVAcPlflTmfb-u9h82hIrQS_dcGEjd6Q */
